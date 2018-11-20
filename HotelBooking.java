251ff1d4452f7c986cc6d3035df95ba7f96f40aa
Gotam Dahiya <44541256+GotamDahiya@users.noreply.github.com>
2018-11-21 04:30:13 +0530

    Frontend

---
+++ HotelBooking.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotelbooking;
+
+/**
+ *
+ * @author praf11
+ */
+public class HotelBooking {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new HotelListing().setVisible(true);
+    }
+    
+}
